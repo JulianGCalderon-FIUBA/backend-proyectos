5ca8a1a912e332b4ef8163c1660cb8815011e4c6
JulianGCalderon <gonzalezcalderonjulian@gmail.com>
2023-06-24 17:15:30 -0300

    hours with default 0

--- src/main/java/com/aninfo/backend/proyectos/controllers/ProjectController.java
+++ src/main/java/com/aninfo/backend/proyectos/controllers/ProjectController.java
@@ -14,7 +14,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
