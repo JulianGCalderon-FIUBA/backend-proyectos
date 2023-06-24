874171f3e52cb8eca3e4e0db643bbb3185774438
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-06-24 14:58:21 -0300

    Cross

--- src/main/java/com/aninfo/backend/proyectos/controllers/ProjectController.java
+++ src/main/java/com/aninfo/backend/proyectos/controllers/ProjectController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
